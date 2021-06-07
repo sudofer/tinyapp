c2dde2b3ae2af3e4aecf5f8989b2e905165a6070
Andres Fernandez-Murray <andres.fmurray@gmail.com>
2021-06-07 20:53:55 +0000

    implemented dedirect after form submition

--- express_server.js
+++ express_server.js
@@ -39,7 +39,10 @@ app.get("/urls", (req, res) => {
 
 app.post("/urls", (req, res) => {
   console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  const shortURL = generateRandomString(6);
+  const longURL = req.body.longURL;
+  urlDatabase[shortURL] = longURL;
+  res.redirect(`/urls/${shortURL}`);
 });
 
 app.get("/urls/new", (req, res) => {
